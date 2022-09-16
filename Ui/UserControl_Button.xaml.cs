96dcbc8236792f0119531cc0bdae755373acfc05
EvgenPotsulyenko <karas@DESKTOP-RFQU3FL>
2022-09-16 13:15:59 +0300

    Добавьте файлы проекта.

---
+++ Ui/UserControl_Button.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_multiLang.Ui
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControl_Button.xaml
+    /// </summary>
+    public partial class UserControl_Button : UserControl
+    {
+        public UserControl_Button()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
